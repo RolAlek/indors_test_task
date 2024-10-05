1088cfd8bee39764390902a2e97e2c7d60855b30
Aleksandr Rol <rolaleksandr75@gmail.com>
2024-10-05 14:16:34 +0900

    add cat model

--- the_cats_app/cats/models.py
+++ the_cats_app/cats/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Cat(models.Model):
+    name = models.CharField(verbose_name='Кличка', max_length=32)
+    color = models.CharField(verbose_name='Окрас',max_length=16)
+    hairiness = models.CharField(verbose_name='Волосатость', max_length=16)
+    birth_date = models.DateField()
+
+    def __str__(self) -> str:
+        return self.name
