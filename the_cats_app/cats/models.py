076f4c6dfc7cb6f77e86fa526fef76c550eac232
Aleksandr Rol <rolaleksandr75@gmail.com>
2024-10-05 14:26:52 +0900

    add owner related ship with user model and apply migration

--- the_cats_app/cats/models.py
+++ the_cats_app/cats/models.py
@@ -1,11 +1,20 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+
+User = get_user_model()
 
 
 class Cat(models.Model):
-    name = models.CharField(verbose_name='Кличка', max_length=32)
-    color = models.CharField(verbose_name='Окрас',max_length=16)
-    hairiness = models.CharField(verbose_name='Волосатость', max_length=16)
+    name = models.CharField(verbose_name="Кличка", max_length=32)
+    color = models.CharField(verbose_name="Окрас", max_length=16)
+    hairiness = models.CharField(verbose_name="Волосатость", max_length=16)
     birth_date = models.DateField()
+
+    owner = models.ForeignKey(
+        User,
+        related_name="cats",
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self) -> str:
         return self.name
