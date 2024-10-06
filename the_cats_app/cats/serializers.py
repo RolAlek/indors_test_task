6c1f5a6b676e79dceac12bba5dc5af795752f944
Aleksandr Rol <rolaleksandr75@gmail.com>
2024-10-06 13:19:06 +0900

    create cat with current user and get_age method

--- the_cats_app/cats/serializers.py
+++ the_cats_app/cats/serializers.py
@@ -1,5 +1,7 @@
-from rest_framework import serializers
+import datetime
+
 import webcolors
+from rest_framework import serializers
 
 from .models import Cat
 
@@ -7,18 +9,43 @@ from .models import Cat
 class HexColorToName(serializers.Field):
     def to_representation(self, value):
         return value
-    
+
     def to_internal_value(self, data):
         try:
             data = webcolors.hex_to_name(data)
         except ValueError as exc:
             raise serializers.ValidationError(
-                'There is no name for this color.'
+                "There is no name for this color."
             ) from exc
         return data
 
+
 class CatSerializer(serializers.ModelSerializer):
     color = HexColorToName()
+    age = serializers.SerializerMethodField()
+
     class Meta:
         model = Cat
-        fields = ["name", "color", "hairiness", "years"]
+        fields = [
+            "name",
+            "color",
+            "hairiness",
+            "birth_year",
+            "owner",
+            "age",
+        ]
+        read_only_fields = ["owner"]
+
+    def create(self, validated_data):
+        validated_data["owner"] = self.context["request"].user
+        return super().create(validated_data)
+
+    def validate_birth_year(self, value):
+        if value > datetime.date.today().year:
+            raise serializers.ValidationError(
+                "Birth year can't be in the future",
+            )
+        return value
+
+    def get_age(self, obj):
+        return datetime.date.today().year - obj.birth_year
--- the_cats_app/cats/views.py
+++ the_cats_app/cats/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpRequest
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from .models import Cat
@@ -8,9 +9,13 @@ from .serializers import CatSerializer
 
 
 @api_view(["GET", "POST"])
+@permission_classes([IsAuthenticated])
 def cat_list(request: HttpRequest) -> Response:
     if request.method == "POST":
-        serializer = CatSerializer(data=request.data)
+        serializer = CatSerializer(
+            data=request.data,
+            context={"request": request},
+        )
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -29,7 +34,7 @@ def cat_detail(request: HttpRequest, pk: int) -> Response:
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    if request.method == 'DELETE':
+    if request.method == "DELETE":
         cat.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
     serializer = CatSerializer(cat)
