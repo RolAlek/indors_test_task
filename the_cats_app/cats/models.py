cb00001c0a670b09db3b375a7f67f1bdd64dc60d
Aleksandr Rol <rolaleksandr75@gmail.com>
2024-10-06 13:17:32 +0900

    add unique constraint for `name` and `owner` fields

--- the_cats_app/cats/models.py
+++ the_cats_app/cats/models.py
@@ -8,13 +8,21 @@ class Cat(models.Model):
     name = models.CharField(verbose_name="Кличка", max_length=32)
     color = models.CharField(verbose_name="Окрас", max_length=16)
     hairiness = models.CharField(verbose_name="Волосатость", max_length=16)
-    years = models.IntegerField(verbose_name="Возраст")
+    birth_year = models.IntegerField(verbose_name="Год")
 
     owner = models.ForeignKey(
         User,
         related_name="cats",
         on_delete=models.CASCADE,
     )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['name', 'owner'],
+                name='unique_name_owner',
+            ),
+        ]
 
     def __str__(self) -> str:
         return self.name
