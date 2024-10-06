dd4dd8f382a4ff87d9fe6df63bf93fc317b94dd4
Aleksandr Rol <rolaleksandr75@gmail.com>
2024-10-06 15:27:39 +0900

    add pagination to cat

--- the_cats_app/cats/views.py
+++ the_cats_app/cats/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpRequest
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.response import Response
 
 from .models import Cat
@@ -20,8 +21,12 @@ def cat_list(request: HttpRequest) -> Response:
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    paginator = LimitOffsetPagination()
     cats = Cat.objects.all()
-    serializer = CatSerializer(cats, many=True)
+    serializer = CatSerializer(
+        paginator.paginate_queryset(cats, request),
+        many=True,
+    )
     return Response(serializer.data)
 
 
