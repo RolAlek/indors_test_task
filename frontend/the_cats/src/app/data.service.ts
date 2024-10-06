d94d6379301c36b3d4e6d4aa5f1705f6e2253bf7
Aleksandr Rol <rolaleksandr75@gmail.com>
2024-10-07 07:18:47 +0900

    create data service in frontend

--- frontend/the_cats/src/app/data.service.ts
+++ frontend/the_cats/src/app/data.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  private data: any[] = [];
+
+  constructor(private apiService: ApiService) { }
+
+  getData(): any[] {
+    return this.data;
+  }
+
+  updateData(data: any[]): void {
+    this.data = data;
+  }
+
+  fetchInitialData(): void {
+    this.apiService.getData().subscribe((data: any[]) => {
+      this.updateData(data);
+    });
+  }
 }
